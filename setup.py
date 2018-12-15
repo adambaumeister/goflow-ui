f44bd68e3ed3f134c4869488be80ed7c9eb23470
adam <abigserve@gmail.com>
2018-12-15 16:55:59 +1100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+DEPENDENCIES = [
+    "PyYAML",
+    "Flask",
+    "mysql-connector",
+]
+
+setup(name="goflow-ui",
+      version=0.02,
+      description="GoFlow basic user interface",
+      author="Adam Baumeister",
+      author_email="adam.baumeister@csiro.au",
+      packages=find_packages(),
+      install_requires=DEPENDENCIES,
+      scripts=['main.py'],
+      )
+
